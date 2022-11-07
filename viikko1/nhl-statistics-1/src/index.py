e248e04107f676580309372ac29d80a6eaaeff39
emma <emmasofia@live.fi>
2022-11-07 23:46:20 +0200

    14-17

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -1,11 +1,16 @@
-from statistics import Statistics
 from player_reader import PlayerReader
+from statistics import Statistics
+from enum import Enum
 
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 def main():
-    stats = Statistics(PlayerReader())
-    philadelphia_flyers_players = stats.team("PHI")
-    top_scorers = stats.top(10)
+    statistics = Statistics(PlayerReader())
+    philadelphia_flyers_players = statistics.team("PHI")
+    top_scorers = statistics.top(10)
 
     print("Philadelphia Flyers:")
     for player in philadelphia_flyers_players:
@@ -14,7 +19,14 @@ def main():
     print("Top point getters:")
     for player in top_scorers:
         print(player)
+    
+    print("Top point goal scorers:")
+    for player in statistics.top(10, SortBy.GOALS.value):
+        print(player)
 
+    print("Top by assists:")
+    for player in statistics.top(10, SortBy.ASSISTS.value):
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko1/nhl-statistics-1/src/statistics.py
+++ viikko1/nhl-statistics-1/src/statistics.py
@@ -1,8 +1,20 @@
 from player_reader import PlayerReader
+from enum import Enum
+
 
 
 def sort_by_points(player):
     return player.points
+
+def sort_by_points(player):
+    return player.points
+
+def sort_by_goals(player):
+    return player.goals
+
+def sort_by_assists(player):
+    return player.assists
+
 
 
 class Statistics:
@@ -26,16 +38,29 @@ class Statistics:
 
         return list(players_of_team)
 
-    def top(self, how_many):
-        sorted_players = sorted(
+    def top(self, how_many, val=1):
+        if val == 1:
+            sorted_players = sorted(
             self._players,
             reverse=True,
             key=sort_by_points
-        )
+            )
+        if val == 2:
+            sorted_players = sorted(
+            self._players,
+            reverse=True,
+            key=sort_by_goals
+            )
+        if val == 3:
+            sorted_players = sorted(
+            self._players,
+            reverse=True,
+            key=sort_by_assists
+            )
 
         result = []
         i = 0
-        while i <= how_many:
+        while i < how_many:
             result.append(sorted_players[i])
             i += 1
 
