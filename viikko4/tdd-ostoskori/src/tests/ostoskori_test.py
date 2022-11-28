2c43388f0da8a7a6a69f4b24427d3c585d3725e0
emma <emmasofia@live.fi>
2022-11-28 21:25:56 +0200

    5.11: kahden saman ostoksen jälkeen yksi olio ostoskorissa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -59,3 +59,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.leipa)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosoliota(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
