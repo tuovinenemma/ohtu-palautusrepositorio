3cc7f5d835c9359d259111d9a8070391064d6ef5
emma <emmasofia@live.fi>
2022-11-28 21:45:45 +0200

    tehtävän 5 siistiminen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -10,20 +10,13 @@ class Ostoskori:
         for ostos in self._ostokset:
             summa += ostos._lukumaara
 
-        return summa
-            
-
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+        return summa    
 
     def hinta(self):
         summa = 0
         for ostos in self._ostokset:
             summa += ostos.hinta()
         return summa
-
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         for ostos in self._ostokset:
@@ -47,5 +40,3 @@ class Ostoskori:
 
     def ostokset(self):
         return self._ostokset
-        # palauttaa listan jossa on korissa olevat ostos-oliot
-        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
